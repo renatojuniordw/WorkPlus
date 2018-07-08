6a650b10878ab6ecf9cfb0f021b5e8f95b322138
Renato Junior <renato-bgs@live.com>
2018-07-07 22:24:18 -0300

    Primeiro commit

---
+++ js/login.js
@@ -0,0 +1,6 @@
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
